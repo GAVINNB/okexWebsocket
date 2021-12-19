68ed7d17386c3106cde36d877e964aa181c35e3e
GAVINNB <15899770656@163.com>
2021-12-19 23:14:43 +0800

    fix

---
+++ api/api.go
@@ -0,0 +1,37 @@
+package api
+
+import (
+	"context"
+	"github.com/GAVINNB/okexWebsocket"
+	"github.com/GAVINNB/okexWebsocket/api/rest"
+	"github.com/GAVINNB/okexWebsocket/api/ws"
+)
+
+// Client is the main api wrapper of okex
+type Client struct {
+	Rest *rest.ClientRest
+	Ws   *ws.ClientWs
+	ctx  context.Context
+}
+
+// NewClient returns a pointer to a fresh Client
+func NewClient(ctx context.Context, apiKey, secretKey, passphrase string, destination okex.Destination) (*Client, error) {
+	restURL := okex.RestURL
+	wsPubURL := okex.PublicWsURL
+	wsPriURL := okex.PrivateWsURL
+	switch destination {
+	case okex.AwsServer:
+		restURL = okex.AwsRestURL
+		wsPubURL = okex.AwsPublicWsURL
+		wsPriURL = okex.AwsPrivateWsURL
+	case okex.DemoServer:
+		restURL = okex.DemoRestURL
+		wsPubURL = okex.DemoPublicWsURL
+		wsPriURL = okex.DemoPrivateWsURL
+	}
+
+	r := rest.NewClient(apiKey, secretKey, passphrase, restURL, destination)
+	c := ws.NewClient(ctx, apiKey, secretKey, passphrase, map[bool]okex.BaseURL{true: wsPriURL, false: wsPubURL})
+
+	return &Client{r, c, ctx}, nil
+}
